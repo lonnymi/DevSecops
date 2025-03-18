86af9168b3078888afb74529cb95d9ca88adba09
Lonny <lonnymi971@gmail.com>
2025-03-18 16:43:23 +0100

    first commit

---
+++ src/main/java/com/example/Dev2Scops/Dev2ScopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Dev2Scops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dev2ScopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dev2ScopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Dev2Scops/Dev2ScopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Dev2Scops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dev2ScopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
